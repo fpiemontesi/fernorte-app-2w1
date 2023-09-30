d352eb57689b5959c92c0275e128f1e91e914929
Franco Piemontesi <franco219011@gmail.com>
2023-09-30 20:04:24 -0300

    Generate invoice component empty

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
+import { GenerateInvoiceComponent } from './generate-invoice/generate-invoice.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, GenerateInvoiceComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
