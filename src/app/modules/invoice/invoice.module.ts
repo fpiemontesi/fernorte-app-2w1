98eb74ac73869486a020c0492a23c2cd438de29d
114403-Herrera-Nicolas <129430000+114403-Herrera-Nicolas@users.noreply.github.com>
2023-10-17 21:40:23 -0300

    Added primary color palette

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { RegistrarFacturaComponent } from './components/registrar-factura/registrar-factura.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RegistrarFacturaComponent],
   providers: [],
   imports: [CommonModule],
   exports: [HomeComponent],
