be32dca9d2aebcbe07a6ab3f739c3c60c35f8915
113943-Sager-Fabio <113943@tecnicatura.frc.utn.edu.ar>
2023-10-20 16:44:27 -0300

    Agregando parte de registro

---
+++ src/app/modules/inventory/services/section.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Section} from "../models/section";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SectionService {
+
+  constructor(private http: HttpClient) { }
+
+  create(body: Section): Observable<Section>{
+    return this.http.post<Section>('http://localhost:3000/secciones', body);
+  }
+
+  getById(id: string): Observable<Section>{
+    return this.http.get<Section>(`http://localhost:3000/secciones/${id}`);
+  }
+
+  getAll(): Observable<Section[]>{
+    return this.http.get<Section[]>(`http://localhost:3000/secciones`);
+  }
+}
