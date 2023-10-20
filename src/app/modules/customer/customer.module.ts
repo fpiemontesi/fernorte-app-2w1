96900f1efdc666b4b5072205abe57a1ced54cf6d
113870-Lokman-Matias <113870@tecnicatura.frc.utn.edu.ar>
2023-10-20 00:38:22 -0300

    Second Commit
    Update customer component created

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
 import { FormsModule } from '@angular/forms';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
 
 @NgModule({
-  declarations: [HomeComponent, CreateCustomerComponent],
+  declarations: [HomeComponent, CreateCustomerComponent, UpdateCustomerComponent],
   providers: [],
   imports: [CommonModule, FormsModule],
   exports: [HomeComponent],
