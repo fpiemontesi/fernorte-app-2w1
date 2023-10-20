59564b9164ad94444eead1c00ffce30a78a80df5
110899 Luciana Sarate <110899@tecnicatura.frc.utn.edu.ar>
2023-10-20 11:47:56 -0300

    110899-Pantalla Crud Proveedores

---
+++ src/app/modules/sales/components/modal-component/modal-component.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Proveedor } from '../../models/Proveedor';
+
+@Component({
+  selector: 'fn-modal-component',
+  templateUrl: './modal-component.component.html',
+  styleUrls: ['./modal-component.component.css']
+})
+export class ModalComponentComponent {
+  @Input() data: any;
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  proveedor: Proveedor = new Proveedor("","","","","","","",""); // Crea una instancia del modelo de Proveedor
+
+  onSubmit() {
+    // Aquí puedes enviar los datos del proveedor al servidor o realizar otras acciones
+    console.log('Proveedor cargado:', this.proveedor);
+    this.activeModal.close();
+  }
+}
