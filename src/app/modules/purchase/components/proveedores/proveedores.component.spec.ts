445f919aaa27788a58bc8e036f1d63c7d8ce4fe3
110899 Luciana Sarate <110899@tecnicatura.frc.utn.edu.ar>
2023-10-21 17:43:13 -0300

    110899-CrudProveedores

---
+++ src/app/modules/purchase/components/proveedores/proveedores.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProveedoresComponent } from './proveedores.component';
+
+describe('ProveedoresComponent', () => {
+  let component: ProveedoresComponent;
+  let fixture: ComponentFixture<ProveedoresComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProveedoresComponent]
+    });
+    fixture = TestBed.createComponent(ProveedoresComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
