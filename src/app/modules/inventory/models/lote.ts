525e98ca325e864d324e94174688a77b2a4babd3
= <=>
2023-10-22 18:17:06 -0300

    correccion de tipo id_seccion en modelo lote

--- src/app/modules/inventory/models/lote.ts
+++ src/app/modules/inventory/models/lote.ts
@@ -1,7 +1,7 @@
 export class Lote {
   id: number = 0;
   id_existence: number = 0;
-  id_section: number = 0;
+  id_section: string = '';
   quantity: number = 0;
   due_date: Date = new Date();
   shelf: number = 0;
