d208835f790b6e4584aa750de99a6c8a1ac67d77
113870-Lokman-Matias <113870@tecnicatura.frc.utn.edu.ar>
2023-10-29 18:08:08 -0300

    feat: delete user uploaded

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -5,9 +5,10 @@ import { HomeComponent } from './components/home/home.component';
 import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
 import { FormsModule } from '@angular/forms';
 import { CreateUserComponent } from './components/create-user/create-user.component';
+import { DeleteUserComponent } from './components/delete-user/delete-user.component';
 
 @NgModule({
-  declarations: [HomeComponent, CreateCustomerComponent, CreateUserComponent],
+  declarations: [HomeComponent, CreateCustomerComponent, CreateUserComponent, DeleteUserComponent],
   providers: [],
   imports: [CommonModule, FormsModule],
   exports: [HomeComponent],
