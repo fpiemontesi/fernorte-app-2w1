8a33be0abef3681462d06dc2a7687a7d4a2c0261
113870-Lokman-Matias <113870@tecnicatura.frc.utn.edu.ar>
2023-10-29 17:59:27 -0300

    feat: create user uploaded

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
 import { FormsModule } from '@angular/forms';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
 @NgModule({
-  declarations: [HomeComponent, CreateCustomerComponent],
+  declarations: [HomeComponent, CreateCustomerComponent, CreateUserComponent],
   providers: [],
   imports: [CommonModule, FormsModule],
   exports: [HomeComponent],
