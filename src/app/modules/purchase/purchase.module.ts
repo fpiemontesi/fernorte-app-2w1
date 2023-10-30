a798c50877b0f2373e3be4dba280bd4975e23f26
jonathanaybar <37118310+jonathanaybar@users.noreply.github.com>
2023-10-30 06:16:06 -0300

    merge con ABM_OrdenDeCompra

--- src/app/modules/purchase/purchase.module.ts
+++ src/app/modules/purchase/purchase.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './components/home/home.component';
-import { ModalComponentComponent } from './components/modal-component/modal-component.component';
-import { ProveedoresComponent } from './components/proveedores/proveedores.component';
-import { ModalComponentbajaComponent } from './components/modal-componentbaja/modal-componentbaja.component';
-import { ModalComponenteditarComponent } from './components/modal-componenteditar/modal-componenteditar.component';
+import { ModalComponentComponent } from './components/proveedores/modal-component/modal-component.component';
+import { ProveedoresComponent } from './components/proveedores/proveedores/proveedores.component';
+import { ModalComponentbajaComponent } from './components/proveedores/modal-componentbaja/modal-componentbaja.component';
+import { ModalComponenteditarComponent } from './components/proveedores/modal-componenteditar/modal-componenteditar.component';
+import {NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarItemComponent } from './components/navbar/item-navbar/item-navbar.component';
+
 
 
 @NgModule({
-  declarations: [HomeComponent,ModalComponentComponent, ProveedoresComponent, ModalComponentbajaComponent, ModalComponenteditarComponent],
+  declarations: [HomeComponent,ModalComponentComponent, ProveedoresComponent, ModalComponentbajaComponent, ModalComponenteditarComponent,NavbarComponent, NavbarItemComponent],
   providers: [],
   imports: [CommonModule],
   exports: [HomeComponent],
