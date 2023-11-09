e4e3968058212c2e3839a46180670debea2c07f4
MartinMacielBramoso <110916@tecnicatura.frc.utn.edu.ar>
2023-11-09 11:49:12 -0300

    Stock Control Remito Added
    Componente para modificar un control de stock agregado

--- src/app/modules/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbToast } from '@ng-bootstrap/ng-bootstrap';
+import { ModifyStockControlComponent } from './components/stockControl/modify-stock-control/modify-stock-control.component';
+import { ToastsComponent } from './components/toasts/toasts.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ModifyStockControlComponent, ToastsComponent],
   providers: [],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgbToast],
   exports: [HomeComponent],
 })
 export class InventaryModule {}
--- src/app/modules/inventory/services/stock-control-report.service.ts
+++ src/app/modules/inventory/services/stock-control-report.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StockControlReport } from '../models/StockControlReport';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockControlReportService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {  }
+
+  getById(id: number): Observable<StockControlReport>{
+    return this.http.get<StockControlReport>('http://localhost:3000/stockControlReports/'+id);
+  }
+
+  modify(control: StockControlReport): Observable<StockControlReport>{
+      return this.http.put<StockControlReport>('http://localhost:3000/stockControlReports/'+control.id,control);
+  }
 }
