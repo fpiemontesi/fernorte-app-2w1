3134ada974427e43317d97867552daa4128bdd40
114007-Santiago-Bima <114007@tecnicatura.frc.utn.edu.ar>
2023-11-16 13:22:17 -0300

    añadiendo routing

--- src/app/modules/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory.module.ts
@@ -5,7 +5,20 @@ import { RegistrarRemitoComponent } from './components/remito/registrar-remito/r
 import { RegistrarLotesComponent } from './components/registrar-lotes/registrar-lotes.component';
 import { FormsModule } from '@angular/forms';
 import { ToastsComponent } from './components/toast/toasts.component';
-import { NgbToast } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbNavModule, NgbToast } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'inventory/remitos/registrar',
+    component: RegistrarRemitoComponent,
+  },
+  {
+    path: 'inventory/lotes/registrar',
+    component: RegistrarLotesComponent,
+  },
+];
 
 @NgModule({
   declarations: [HomeComponent, RegistrarLotesComponent, RegistrarRemitoComponent, ToastsComponent],
@@ -13,8 +26,15 @@ import { NgbToast } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     CommonModule,
     FormsModule,
-    NgbToast
+    NgbToast,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    NgbNavModule,
+    NgbDropdownModule
   ],
-  exports: [HomeComponent],
+  exports: [
+    HomeComponent,
+    RouterModule
+  ],
 })
 export class InventaryModule {}
