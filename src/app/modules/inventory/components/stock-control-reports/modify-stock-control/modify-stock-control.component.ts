6b173c75357541c0b82624287d319b40b6d98aa6
113943-Sager-Fabio <113943@tecnicatura.frc.utn.edu.ar>
2023-11-16 20:12:31 -0300

    borrando rounting

--- src/app/modules/inventory/components/stock-control-reports/modify-stock-control/modify-stock-control.component.ts
+++ src/app/modules/inventory/components/stock-control-reports/modify-stock-control/modify-stock-control.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { StockControlReport } from '../../../models/StockControlReport';
-import { Subscription } from 'rxjs';
-import { StockControlReportService } from '../../../services/stock-control-report.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {StockControlReport} from '../../../models/StockControlReport';
+import {Subscription} from 'rxjs';
+import {StockControlReportService} from '../../../services/stock-control-report.service';
 import {ToastService} from "../../../services/toast.service";
 
 @Component({
@@ -10,14 +10,15 @@ import {ToastService} from "../../../services/toast.service";
   templateUrl: './modify-stock-control.component.html',
   styleUrls: ['./modify-stock-control.component.css']
 })
-export class ModifyStockControlComponent implements OnInit, OnDestroy{
+export class ModifyStockControlComponent implements OnInit, OnDestroy {
 
   stockControlForm: FormGroup = new FormGroup({});
   stockControl: StockControlReport = new StockControlReport();
   private subscription: Subscription = new Subscription();
 
   constructor(private formBuilder: FormBuilder, private stockControlService: StockControlReportService,
-              private toastService: ToastService) {}
+              private toastService: ToastService) {
+  }
 
   ngOnInit(): void {
     this.stockControlForm = this.formBuilder.group({
@@ -34,7 +35,7 @@ export class ModifyStockControlComponent implements OnInit, OnDestroy{
     })
   }
 
-  loadStockControlData(){
+  loadStockControlData() {
     this.stockControlForm.patchValue({
       batchId: this.stockControl.batchId,
       description: this.stockControl.description,
@@ -44,25 +45,27 @@ export class ModifyStockControlComponent implements OnInit, OnDestroy{
     });
   }
 
-  updateStockControl(){
-    if(this.stockControlForm.valid){
+  updateStockControl() {
+    if (this.stockControlForm.valid) {
       this.stockControl.batchId = this.stockControlForm.get('batchId')?.value;
       this.stockControl.description = this.stockControlForm.get('description')?.value;
       this.stockControl.inspectedQuantity = this.stockControlForm.get('inspectedQuantity')?.value;
       this.stockControl.damagedQuantity = this.stockControlForm.get('damagedQuantity')?.value;
       this.stockControl.batchIsExpired = this.stockControlForm.get('batchIsExpired')?.value;
 
-      this.stockControlService.modify(this.stockControl).subscribe((updatedControl: StockControlReport) => {
-        this.toastService.show("Formulario valido",{classname: 'bg-success text-light'});
-      });
-    }
-    else{
-      this.toastService.show("Formulario invalido",{classname: 'bg-danger text-light'});
+      this.subscription.add(
+        this.stockControlService.modify(this.stockControl).subscribe((updatedControl: StockControlReport)
+          => {
+          this.toastService.show("Formulario valido", {classname: 'bg-success text-light'});
+        })
+      );
+    } else {
+      this.toastService.show("Formulario invalido", {classname: 'bg-danger text-light'});
     }
   }
 
   ngOnDestroy(): void {
-    if(this.subscription){
+    if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
