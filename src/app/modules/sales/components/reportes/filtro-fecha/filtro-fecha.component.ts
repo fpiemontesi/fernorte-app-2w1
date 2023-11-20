ebe602120b9142d25fcc038c819e243d7a8211da
113905-MELLO-CANDELARIA <113905@tecnicatura.frc.utn.edu.ar>
2023-11-20 02:53:15 -0300

    update reportePorMeses

--- src/app/modules/sales/components/reportes/filtro-fecha/filtro-fecha.component.ts
+++ src/app/modules/sales/components/reportes/filtro-fecha/filtro-fecha.component.ts
@@ -27,117 +27,6 @@ export class FiltroFechaComponent {
     
   }
 
-  
-  // onSubmit() {
-  //   if (!this.formData.anio) {
-  //     console.error('El año es obligatorio para generar el gráfico.');
-  //     return;
-  //   }
-
- 
-  //   this.reporteEstadoVenta();
-  // //  this.reporteTopProductos();
-
-  // }   
-    
-  // reporteEstadoVenta() {
-  //   this.reportesService.getReportes(this.formData.anio, this.formData.mes, this.formData.tipo_venta).subscribe(
-  //     (reportes) => {
-  //       const chartData = {
-  //         labels: ['Pendiente', 'Entregado'], 
-  //         datasets: [
-  //           {
-  //             label: 'Estado de ventas',
-  //             data: [reportes.pendiente, reportes.entregado], 
-  //             backgroundColor: [ 'red', 'green' ] 
-  //           }
-  //         ]
-  //       };
-  
-  //       const canvas = document.getElementById('estado-venta') as HTMLCanvasElement;
-  //       const ctx = canvas.getContext('2d');
-  //       if (ctx) {
-  //         if (this.chartInstance) {
-  //           this.chartInstance.destroy(); 
-  //         }
-          
-  //         this.chartInstance = new Chart(ctx, {
-  //           type: 'bar',
-  //           data: chartData,
-  //           options: {
-  //             scales: {
-  //               y: {
-  //                 beginAtZero: true 
-  //               }
-  //             }
-  //           }
-  //         });
-  //       } else {
-  //         console.error('Failed to get the 2D rendering context for the canvas');
-  //       }
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // }
-  
-    
-  // reporteTopProductos(){
-  //     this.reportesService.getReportes(this.formData.anio, this.formData.mes, this.formData.tipo_venta).subscribe(
-  //       (reportes) => {
-  //         const chartData: { labels: string[], datasets: any[] } = {
-  //           labels: [],
-  //           datasets: [
-  //             {
-  //               label: 'Productos más vendidos',
-  //               data: [],
-  //               backgroundColor: []
-  //             }
-  //           ]
-  //         };
-      
-  //         reportes.productos.forEach((productos: any) => {
-  //           chartData.labels.push(productos.descripcion);
-  //           chartData.datasets[0].data.push(productos.cantidad);
-  //           chartData.datasets[0].backgroundColor.push(this.generateRandomColor(5));
-  //         });
-      
-  //         const canvas = document.getElementById('top-productos') as HTMLCanvasElement;
-  //         const ctx = canvas.getContext('2d');
-  //         if (ctx) {
-  //           if (this.chartInstance) {
-  //             this.chartInstance.destroy();
-  //           }
-            
-  //           this.chartInstance = new Chart(ctx, {
-  //             type: 'pie' as ChartType,
-  //             data: chartData,
-  //             options: {
-  //             }
-  //           });
-  //         } else {
-  //           console.error('Failed to get the 2D rendering context for the canvas');
-  //         }
-  //       },
-  //       (error) => {
-  //         console.error(error);
-  //       }
-  //     );
-      
-  //   }
-    
-
-  //  generateRandomColor(count: number): string[] {
-  //   const colors: string[] = [];
-
-  //   const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)},${count})`;
-  //   colors.push(color);
-
-  //   return colors;
-  // }
-
-
   onSubmit() {
     if (!this.formData.anio) {
       console.error('El año es obligatorio para generar el gráfico.');
@@ -146,19 +35,27 @@ export class FiltroFechaComponent {
   
     this.getReportesAndGenerateCharts();
   }
+
+  response:any;
   
   getReportesAndGenerateCharts() {
     this.reportesService.getReportes(this.formData.anio, this.formData.mes, this.formData.tipo_venta).subscribe(
       (reportes) => {
-        // this.generarReporteTopProductos(reportes);
-        //  this.generarReporteEstadoVenta(reportes);
-        // this.generarReporteVentasPorMeses(reportes);
-        // this.generarReporteTipoVentas(reportes);
+        this.response = reportes;
+        this.generarReportes(reportes);
+       
       },
       (error) => {
         console.error(error);
       }
     );
+  }
+
+  generarReportes(response: any) {
+    // this.generarReporteTopProductos(response);
+        //  this.generarReporteEstadoVenta(response);
+        this.generarReporteVentasPorMeses(response);
+        // this.generarReporteTipoVentas(response);
   }
   
   generarReporteTipoVentas(reportes : any) {
@@ -194,14 +91,29 @@ export class FiltroFechaComponent {
     } else {
       console.error('Error al generar los reportes de ventas del mes anterior y del mes actual.');  }
   }
-  generarReporteVentasPorMeses(reportes : any) {
+  generarReporteVentasPorMeses(reportes: any) {
     const chartData = {
-      labels: ['Mes Anterior', 'Mes Actual'], 
+      labels: ['Mes Anterior', 'Mes Actual'],
       datasets: [
         {
-          label: 'Venta del Mes Anterior y del Mes Actual',
-          data: [reportes.total_mes_anterior, reportes.total_ventas], 
-          backgroundColor: this.generateRandomColor(2)
+          label: 'Mes Anterior (Monto)',
+          data: [reportes.monto_mes_anterior],
+          backgroundColor: this.generateRandomColor(1)
+        },
+        {
+          label: 'Mes Anterior (Cant Ventas)',
+          data: [reportes.total_mes_anterior],
+          backgroundColor: this.generateRandomColor(1)
+        }, 
+        {
+          label: 'Mes Actual (Cant Ventas)',
+          data: [reportes.total_ventas],
+          backgroundColor: this.generateRandomColor(1)
+        },
+        {
+          label: 'Mes Actual (Monto)',
+          data: [reportes.monto],
+          backgroundColor: this.generateRandomColor(1)
         }
       ]
     };
@@ -210,23 +122,26 @@ export class FiltroFechaComponent {
     const ctx = canvas.getContext('2d');
     if (ctx) {
       if (this.chartInstance) {
-        this.chartInstance.destroy(); 
+        this.chartInstance.destroy();
       }
-      
+  
       this.chartInstance = new Chart(ctx, {
         type: 'bar',
-            data: chartData,
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true 
-                }
-              }
+        data: chartData,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
             }
+          }
+        }
       });
     } else {
-      console.error('Error al generar los reportes de ventas del mes anterior y del mes actual.');  }
-  }
+      console.error('Error al generar los reportes de ventas del mes anterior y del mes actual.');
+    }
+  } 
+  
+  
   
   generarReporteEstadoVenta(reportes : any) {
     const chartData = {
