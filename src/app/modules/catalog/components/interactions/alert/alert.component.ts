b7b22b36d39bdc4ea0de8bc0984980d022ef21b8
113935-Quilpatay-Nahuel-Ignacio <113935@tecnicatura.frc.utn.edu.ar>
2023-11-23 19:19:46 -0300

    alert+modal

--- src/app/modules/catalog/components/interactions/alert/alert.component.ts
+++ src/app/modules/catalog/components/interactions/alert/alert.component.ts
@@ -1,12 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fn-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent implements OnInit{
-  showAlert:boolean= true;
+export class AlertComponent implements OnInit {
+  showAlert: boolean = true;
   @Input() mensaje = "";
   @Input() tipoAlert = "";
 
@@ -15,7 +15,4 @@ export class AlertComponent implements OnInit{
       this.showAlert = false;
     }, 5000);
   }
-
-  
-
 }
--- src/app/modules/catalog/components/interactions/modal/modal.component.ts
+++ src/app/modules/catalog/components/interactions/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'fn-modal',
@@ -7,13 +7,12 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class ModalComponent {
   @Output() evento = new EventEmitter()
-  @Input() title:string=""
-  @Input() mensaje:string=""
-  @Input() option1:string=""
-  @Input() option2:string=""
+  @Input() title: string = ""
+  @Input() mensaje: string = ""
+  @Input() option1: string = ""
+  @Input() option2: string = ""
 
-  onEvent(){
+  onEvent() {
     this.evento.emit()
   }
-
 }
