bc273dccd74f2f501d492571a20753895ab618af
113935-Quilpatay-Nahuel-Ignacio <113935@tecnicatura.frc.utn.edu.ar>
2023-11-23 19:21:21 -0300

    home

--- src/app/modules/catalog/components/home/home.component.ts
+++ src/app/modules/catalog/components/home/home.component.ts
@@ -5,14 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
 
   constructor() {
   }
-
-
-
 }
