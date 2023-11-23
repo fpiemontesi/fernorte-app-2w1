27fca6f2dda4647189f1d46a2abafdb22e734c81
113935-Quilpatay-Nahuel-Ignacio <113935@tecnicatura.frc.utn.edu.ar>
2023-11-23 19:25:54 -0300

    product

--- src/app/modules/catalog/components/crud-product/delete-product-list/delete-product.component.ts
+++ src/app/modules/catalog/components/crud-product/delete-product-list/delete-product.component.ts
@@ -1,38 +1,38 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Producto} from "../../../models/producto";
-import {productService} from "../../../services/productService/product.service";
-import {Subscription} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Producto } from "../../../models/producto";
+import { productService } from "../../../services/productService/product.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'fn-delete-product-list',
   templateUrl: './delete-product.component.html',
   styleUrls: ['./delete-product.component.css']
 })
-export class DeleteProductComponent implements OnInit,OnDestroy{
+export class DeleteProductComponent implements OnInit, OnDestroy {
 
-  constructor(private httpClientService:productService) {
+  constructor(private httpClientService: productService) {
   }
+
   title = 'Modificar Producto ';
-  products:Producto[] = [];
+  products: Producto[] = [];
   private subscription = new Subscription();
 
   ngOnInit(): void {
     this.getProductsActive();
   }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  getProductsActive(){
+  getProductsActive() {
     this.subscription.add(
       this.httpClientService.getAllProducts().subscribe(
-        (response:Producto[])=>{
-          this.products=response;
+        (response: Producto[]) => {
+          this.products = response;
         })
     )
   }
-
-
 
   deleteProduct(id: string) {
     const confirmed = confirm("¿Desea eliminar el producto?")
@@ -50,7 +50,4 @@ export class DeleteProductComponent implements OnInit,OnDestroy{
       )
     }
   }
-
-
-
 }
--- src/app/modules/catalog/components/crud-product/register-product/register-product.component.ts
+++ src/app/modules/catalog/components/crud-product/register-product/register-product.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { StorageService } from '../../../services/storage/storage.service';
-import {productService} from "../../../services/productService/product.service";
-import {Producto} from "../../../models/producto";
-import {Subscription} from "rxjs";
+import { productService } from "../../../services/productService/product.service";
+import { Producto } from "../../../models/producto";
+import { Subscription } from "rxjs";
 import { Categoria } from '../../../models/categoria';
 import { CategoriaService } from '../../../services/categoryService/categoria.service';
 import { Marca } from '../../../models/marca';
@@ -22,226 +22,216 @@ export class RegisterProductComponent implements OnInit, OnDestroy {
   productsInactives: Producto[] = []
   categoriesNombre: String[] = []
   prodPreCarga: ProductoPreCarga = {} as ProductoPreCarga;
-  allModels:Producto[]=[];
+  allModels: Producto[] = [];
   marcas: Marca[] = [];
   codeProductSelected = "";
   title = 'Modificar Producto';
-  model:Producto = {} as Producto;
+  model: Producto = {} as Producto;
   codigo = "";
   flag = false;
   private subscription = new Subscription();
 
-    constructor(
-      private storageService: StorageService,
-      private httpClientService:productService,
-      private httpClientCategoria: CategoriaService,
-      private htppBrand: ServiceMarcaService,
-      private router: Router,
-      private activedRoute: ActivatedRoute
-      ) { }
+  constructor(
+    private storageService: StorageService,
+    private httpClientService: productService,
+    private httpClientCategoria: CategoriaService,
+    private htppBrand: ServiceMarcaService,
+    private router: Router,
+    private activedRoute: ActivatedRoute
+  ) { }
 
-
-
-    ngOnInit(): void {
-
-
-      //TODO: get params from url
-      this.subscription.add(
-        this.activedRoute.queryParams.subscribe(params => {
-          this.codigo = params["codigo"];
-          this.title = params["title"];
-          if(this.codigo == null || undefined){
-            this.cargarDatos();
-            this.title = "Modificar Producto";
-          }
-          console.log(this.codigo);
-           if(this.codigo != null || undefined){
-            this.subscription.add(
-             this.httpClientService.getProductByCode(this.codigo).subscribe(
+  ngOnInit(): void {
+    this.subscription.add(
+      this.activedRoute.queryParams.subscribe(params => {
+        this.codigo = params["codigo"];
+        this.title = params["title"];
+        if (this.codigo == null || undefined) {
+          this.cargarDatos();
+          this.title = "Modificar Producto";
+        }
+        console.log(this.codigo);
+        if (this.codigo != null || undefined) {
+          this.subscription.add(
+            this.httpClientService.getProductByCode(this.codigo).subscribe(
               {
-                next: (response:Producto)=>{
+                next: (response: Producto) => {
                   this.productsInactives = [];
-                  this.model=response;
+                  this.model = response;
                   this.productsInactives.push(this.model);
-                  if(this.productsInactives[0].codigo == null || undefined){
+                  if (this.productsInactives[0].codigo == null || undefined) {
                     console.log("No existe el producto");
                     this.router.navigate(["/listProducts"]);
                   }
 
                   console.log(this.productsInactives);
                 },
-                error: (error:any)=>{
+                error: (error: any) => {
                   console.log(error);
                 },
-                complete:()=>{
+                complete: () => {
                   console.log("Petición realizada");
                 }
 
               }
-             )
             )
+          )
+        }
+      }),
+    )
 
-           }
-        }),
-      )
-
-
-
-
-      this.subscription.add(
-        this.httpClientService.getAllProducts().subscribe(
-          (response:Producto[])=>{
-            this.allModels=response;
-            }
-        )
-      )
-
-
-      this.subscription.add(
-        this.httpClientCategoria.get().subscribe(
-          (response:Categoria[])=>{
-            this.categorias=response;
-            }
-        )
-      )
-
-      this.subscription.add(
-        this.htppBrand.get().subscribe(
-          (response:Marca[])=>{
-            this.marcas=response;
-            }
-        )
-      )
-    }
-
-    ngOnDestroy() {
-      this.subscription.unsubscribe();
-    }
-
-  getModelByCode(){
     this.subscription.add(
-      this.httpClientService.getProductByCode(this.codeProductSelected).subscribe(
-        (response:Producto)=>{
-          this.model=response;
+      this.httpClientService.getAllProducts().subscribe(
+        (response: Producto[]) => {
+          this.allModels = response;
+        }
+      )
+    )
+
+    this.subscription.add(
+      this.httpClientCategoria.get().subscribe(
+        (response: Categoria[]) => {
+          this.categorias = response;
+        }
+      )
+    )
+
+    this.subscription.add(
+      this.htppBrand.get().subscribe(
+        (response: Marca[]) => {
+          this.marcas = response;
         }
       )
     )
   }
 
-    formArticle = new FormGroup({
-      codigo_marca: new FormControl('', [Validators.required]),
-      categorias_id: new FormControl('', [Validators.required, Validators.nullValidator]),
-      descripcion: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]),
-      dimensiones: new FormControl("", [Validators.required]), // Inicializado en 0 como número
-      peso: new FormControl("", [Validators.required]), // Inicializado en 0 como número
-      color: new FormControl('', [Validators.required]),
-      material: new FormControl('', [Validators.required]),
-      pais: new FormControl('', [Validators.required]),
-      imageURL: new FormControl('', [Validators.required]),
-    });
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
-
-
-
-
-
-    addElement(){
-      if(this.formArticle.value.categorias_id != ("" || null || undefined)){
-
-        for(let i = 0; i < this.categoriesCodigo.length; i++){
-          if(this.categoriesCodigo[i] == this.formArticle.value.categorias_id){
-            alert("Ya se ha agregado esta categoría");
-            return;
-          }
+  getModelByCode() {
+    this.subscription.add(
+      this.httpClientService.getProductByCode(this.codeProductSelected).subscribe(
+        (response: Producto) => {
+          this.model = response;
         }
-        this.categoriesCodigo.push(this.formArticle.value.categorias_id);
-        this.categoriesNombre.push(this.categorias.find(categoria => categoria.codigo == this.formArticle.value.categorias_id)?.nombre || "");
-      }
-    }
+      )
+    )
+  }
+
+  formArticle = new FormGroup({
+    codigo_marca: new FormControl('', [Validators.required]),
+    categorias_id: new FormControl('', [Validators.required, Validators.nullValidator]),
+    descripcion: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]),
+    dimensiones: new FormControl("", [Validators.required]),
+    peso: new FormControl("", [Validators.required]),
+    color: new FormControl('', [Validators.required]),
+    material: new FormControl('', [Validators.required]),
+    pais: new FormControl('', [Validators.required]),
+    imageURL: new FormControl('', [Validators.required]),
+  });
 
 
-    removeCategory(index: number){
-      this.categoriesNombre.splice(index, 1);
-      this.categoriesCodigo.splice(index, 1);
-    }
 
 
-    onSubmit() {
-      if(this.formArticle.valid){
-        this.prodPreCarga = {
-          activo: true,
-          codigo_categorias: this.categoriesCodigo as string[],
-          codigo_marca: this.formArticle.value.codigo_marca || "",
-          paisOrigen: this.formArticle.value.pais || "",
-          color: this.formArticle.value.color || "",
-          material: this.formArticle.value.material || "",
-          peso: this.formArticle.value.peso || "",
-          dimensiones: this.formArticle.value.dimensiones || "",
-          imageURL: this.formArticle.value.imageURL || "",
-          precio_compra: 0,
-          descripcion: this.formArticle.value.descripcion || "",
+
+
+  addElement() {
+    if (this.formArticle.value.categorias_id != ("" || null || undefined)) {
+
+      for (let i = 0; i < this.categoriesCodigo.length; i++) {
+        if (this.categoriesCodigo[i] == this.formArticle.value.categorias_id) {
+          alert("Ya se ha agregado esta categoría");
+          return;
         }
-
-        let flag = false;
-
-        //TODO: add alert of success
-        this.subscription.add(
-          this.httpClientService.updateProduct(this.productsInactives[0].codigo, this.prodPreCarga ).subscribe(
-            (response:Producto)=>{
-              this.model=response;
-              alert("Producto creado");
-              flag = true;
-              this.formArticle.reset();
-              this.categoriesCodigo = [];
-              this.categoriesNombre = [];
-              this.cargarDatos();
-
-            }
-          )
-        )
-
-      }else{
-        alert("Rellene todos los campos");
       }
+      this.categoriesCodigo.push(this.formArticle.value.categorias_id);
+      this.categoriesNombre.push(this.categorias.find(categoria => categoria.codigo == this.formArticle.value.categorias_id)?.nombre || "");
     }
+  }
 
-    cargarDatos(){
+
+  removeCategory(index: number) {
+    this.categoriesNombre.splice(index, 1);
+    this.categoriesCodigo.splice(index, 1);
+  }
+
+
+  onSubmit() {
+    if (this.formArticle.valid) {
+      this.prodPreCarga = {
+        activo: true,
+        codigo_categorias: this.categoriesCodigo as string[],
+        codigo_marca: this.formArticle.value.codigo_marca || "",
+        paisOrigen: this.formArticle.value.pais || "",
+        color: this.formArticle.value.color || "",
+        material: this.formArticle.value.material || "",
+        peso: this.formArticle.value.peso || "",
+        dimensiones: this.formArticle.value.dimensiones || "",
+        imageURL: this.formArticle.value.imageURL || "",
+        precio_compra: 0,
+        descripcion: this.formArticle.value.descripcion || "",
+      }
+
+      let flag = false;
+
+      //TODO: add alert of success
       this.subscription.add(
-        this.httpClientService.getProductsInactives().subscribe({
-          next: (response:Producto[])=>{
-            console.log(response);
-            this.productsInactives=response;
-            },
-            error: (error:any)=>{
-              console.log(error);
-            },
-            complete:()=>{
-              console.log("Petición realizada");
-            }
-        }
+        this.httpClientService.updateProduct(this.productsInactives[0].codigo, this.prodPreCarga).subscribe(
+          (response: Producto) => {
+            this.model = response;
+            alert("Producto creado");
+            flag = true;
+            this.formArticle.reset();
+            this.categoriesCodigo = [];
+            this.categoriesNombre = [];
+            this.cargarDatos();
+
+          }
         )
       )
+
+    } else {
+      alert("Rellene todos los campos");
     }
+  }
 
-
-    resetFields(){
-      alert("SUCCESS");
-      this.router.navigate(["/deleteProduct"])
-    }
-
-    readPhoto(event:any){
-      const archivoCapturado = event.target.files[0];
-      if (archivoCapturado instanceof Blob) {
-        let reader = new FileReader();
-        reader.readAsDataURL(archivoCapturado);
-        reader.onloadend = async () => {
-          const imagen = reader.result;
-          this.formArticle.value.imageURL = await this.storageService.subirImagen(archivoCapturado);
-        };
+  cargarDatos() {
+    this.subscription.add(
+      this.httpClientService.getProductsInactives().subscribe({
+        next: (response: Producto[]) => {
+          console.log(response);
+          this.productsInactives = response;
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log("Petición realizada");
+        }
       }
-      else {
-        this.formArticle.value.imageURL = "";
-      }
+      )
+    )
+  }
+
+
+  resetFields() {
+    alert("SUCCESS");
+    this.router.navigate(["/deleteProduct"])
+  }
+
+  readPhoto(event: any) {
+    const archivoCapturado = event.target.files[0];
+    if (archivoCapturado instanceof Blob) {
+      let reader = new FileReader();
+      reader.readAsDataURL(archivoCapturado);
+      reader.onloadend = async () => {
+        const imagen = reader.result;
+        this.formArticle.value.imageURL = await this.storageService.subirImagen(archivoCapturado);
+      };
     }
+    else {
+      this.formArticle.value.imageURL = "";
+    }
+  }
 }
 
